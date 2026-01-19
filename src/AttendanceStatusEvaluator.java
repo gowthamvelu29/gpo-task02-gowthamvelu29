f5f2e59cb1f5c511b068128604e3ce2a2c765acd
Gowtham <gowthamvelu2909@gmail.com>
2026-01-19 15:53:35 +0530

    Update AttendanceStatusEvaluator.java

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -1,1 +1,17 @@
-
+import java.util.Scanner;
+public class Attendance{
+  public static void main(String[] args){
+    Scanner scanner=new Scanner(System.in);
+    Scanner.close();
+    int attendance=scanner.nextInt();
+    if(attendance>80){
+      System.out.println("Excellent");
+    }
+    else if(attendance>=60){
+      System.out.println("Satisfactory");
+      }
+    else{
+      System.out.println("Poor");
+    }
+  }
+}
